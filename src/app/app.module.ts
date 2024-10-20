d32bf2e54cb929f4a456c851d606fbf3db47c9e0
Jatin <Jatinjihere07@gmail.com>
2024-10-20 18:14:38 +0530

    Updated Path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { PrimeNGModule } from './primeng/primeng.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
@@ -11,16 +10,10 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { VerifyCodeComponent } from './features/auth/verify-code/verify-code.component';
 import { LoginComponent } from './features/auth/login/login.component';
-import { AuthenticationService } from './core/services/authentication.service';
-import { ConfigService } from './core/services/config.service';
-import { ApiInterceptor } from './core/interceptor/api.interceptor';
-import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { ApiInterceptor } from './core/interceptor/api.interceptor'
 import { RouterModule } from '@angular/router';
 
 
