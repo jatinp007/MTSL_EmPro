4a1344892fdf6fa232df7e2c77fa4ac31fbc6b79
shivrajdeopa11@gmail.com <shivrajdeopa11@gmail.com>
2024-10-22 16:57:44 +0530

    add icons

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -9,11 +9,12 @@ import { ApplicationRoutes } from '../../shared/enums/routes.enum';
 export class SidebarComponent {
   menuItems = [
     { icon: 'pi-th-large', label: 'Dashboard', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-users', label: 'Employees', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-calendar', label: 'Calendar', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-clock', label: 'Time', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-users', label: 'Teams', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-file', label: 'Documents', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
-    { icon: 'pi-chart-bar', label: 'Reports', route: ApplicationRoutes.PURCHASE_ORDER_LIST }
+    { icon: 'pi-receipt', label: 'Purchase', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-calendar', label: 'Sales', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-user', label: 'Vendor', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-tag', label: 'Material', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-file', label: 'Masters', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-chart-bar', label: 'Reports', route: ApplicationRoutes.PURCHASE_ORDER_LIST },
+    { icon: 'pi-warehouse', label: 'Inventory', route: ApplicationRoutes.PURCHASE_ORDER_LIST }
   ];
 }
